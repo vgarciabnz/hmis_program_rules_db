e380f6a946b93ad4d03f46356a45f2fb45100cc7
Victor Garcia <vgarciabnz@gmail.com>
2017-07-03 15:47:01 +0200

    Fix bat script

--- HMIS_program_rules.bat
+++ HMIS_program_rules.bat
@@ -3,6 +3,7 @@
 SET HMIS_HOME=C:\Program Files (x86)\DHIS2
 SET DHIS2_HOME=%HMIS_HOME%\DHIS
 SET FILES_HOME=%DHIS2_HOME%\files
+SET PGPASSFILE=%HMIS_HOME%\pgpass.conf
 
 SET COMMON_FUNCTIONS_SQL=common_function.sql
 SET MENTAL_HEALTH_SQL=mental_health.sql
@@ -22,10 +23,9 @@ FOR /F "usebackq eol=; tokens=1,2* delims==" %%i IN ("%DHIS2_HOME%\dhis.conf") D
 		)
 
 		IF "%1%"=="hibernate.connection.url" (
-			SET FULL_URL=%2
-
 			:: Let's suppose the url has a syntax like "jdbc:postgresql://localhost:5544/dhis2"
-			FOR /F "tokens=1,2,3,4,5 delims=/:" %%a in ("%FULL_URL%") do (
+			FOR /F "tokens=1,2,3,4,5 delims=/:" %%a in ("%2%") do (
+				echo %%a
 				SET DHIS2_HOST=%%c
 				SET DHIS2_DB_PORT=%%d
 				SET DHIS2_DB_NAME=%%e
@@ -42,7 +42,7 @@ ECHO %DHIS2_HOST%
 ECHO %DHIS2_DB_PORT%
 ECHO %DHIS2_DB_NAME%
 
-SET PSQL_EXE="%HMIS_HOME%\pgsql\bin\psql.exe" -t -U %DHIS2_DB_USERNAME% -d %DHIS2_DB_NAME% -p %DHIS2_DB_PORT%
+SET PSQL_EXE="%HMIS_HOME%\pgsql\bin\psql.exe" -t -U %DHIS2_DB_USERNAME% -d %DHIS2_DB_NAME% -p %DHIS2_DB_PORT% -w
 
 :loop_sql
 
